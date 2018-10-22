8bee73b813f1dc8ae77ba8aa20e9d5300c810a6f
Дмитрий Капустюк <dima.kapustiuk@gmail.com>
2018-10-22 20:51:26 +0300

    created model and service

--- project/src/index.js
+++ project/src/index.js
@@ -2,7 +2,6 @@ import Model from './js/model';
 import View from './js/view';
 import Controller from './js/controller';
 import styles from './sass/styles.scss';
-import TestGitHub from './js/dima.js'
 
 
 const view = new View();
@@ -11,9 +10,27 @@ const model = new Model();
 
 new Controller(view, model);
 
-const test = new TestGitHub('Git hab test worked');
+const request = {
+  query: 'cat',
+  page: 1
+}
 
-test.consoleDimaText();
+const item = {
+  id: 2083492,
+  largeImageURL: "https://pixabay.com/get/eb35b90c2cfd033ed1584d05fb1d4796e771e3d61cb00c4090f4c17ba6e5b2bade_1280.jpg",
+  pageURL: "https://pixabay.com/en/cat-young-animal-curious-wildcat-animal--2083492/",
+}
+
+model.getImages(request)
+
+console.log(model.selecteds);
+
+model.addToSelected(item)
+
+console.log(model.selecteds)
+
+model.clearSelecteds()
+
 
 
 

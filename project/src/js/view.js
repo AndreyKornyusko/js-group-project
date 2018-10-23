0d1ce69267f2b1fdedc4020d12cc68bec7b94f29
Yegor <georg.polski@gmail.com>
2018-10-23 13:02:39 +0300

    fixed index.html and view

--- project/src/js/view.js
+++ project/src/js/view.js
@@ -1,28 +1,21 @@
 import card from '../templates/cards.hbs';
 
 export default class View {
-  constructor() {
-    this.refs = {};
-        this.refs.selectedBtn = document.querySelector('.js-selected-btn');
-        this.refs.form = document.querySelector('.js-form');
-        this.refs.input = document.querySelector("input[name=query]");
-        this.refs.searchBtn = document.querySelector("button[data-action=search]");
-        this.refs.list = document.querySelector('.img-list');
-        this.refs.images = document.querySelectorAll('.img-card')
-        this.refs.source = document.querySelector('#js_markup').innerHTML.trim();
-        this.refs.template = Handlebars.compile(this.refs.source);
+ constructor() {
+   this.refs = {};
+   this.refs.selectedBtn = document.querySelector('.selected-btn');
+   this.refs.form = document.querySelector('.js-form');
+   this.refs.input = document.querySelector('input[name=query]');
+   this.refs.searchBtn = document.querySelector('button[data-action=search]');
+   this.refs.list = document.querySelector('.list__img-card');
+   this.refs.images = document.querySelectorAll('.list__img-card');
+   this.refs.container = document.querySelector('#root');
 
-  }
-  createList(items) {
-    const markup = items.reduce( (acc, el) => acc + this.refs.template( {
-      id: el.id,
-      image: el.src,
-      description: el.description
-    }), "" );
-    this.refs.list.insertAdjacentHTML(
-      "afterbegin",
-      markup
-    );
-  }
+ }
 
+ createTemplate(arr) {
+   const markup = arr.reduce((acc, item) => acc + card(item), '');
+
+   this.refs.container.insertAdjacentHTML('afterbegin', markup);
+ }
 }

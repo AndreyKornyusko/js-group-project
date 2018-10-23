b8f0e193c061dbbdf91279b4e4c7a102e9c31fe4
Yegor <georg.polski@gmail.com>
2018-10-23 13:18:36 +0300

    updated html, view and templates

--- project/src/js/view.js
+++ project/src/js/view.js
@@ -3,19 +3,18 @@ import card from '../templates/cards.hbs';
 export default class View {
  constructor() {
    this.refs = {};
-   this.refs.selectedBtn = document.querySelector('.selected-btn');
+   this.refs.selectedBtn = document.querySelector('.selected__button');
    this.refs.form = document.querySelector('.js-form');
    this.refs.input = document.querySelector('input[name=query]');
    this.refs.searchBtn = document.querySelector('button[data-action=search]');
-   this.refs.list = document.querySelector('.list__img-card');
-   this.refs.images = document.querySelectorAll('.list__img-card');
+   this.refs.images = document.querySelectorAll('.list__item--img');
    this.refs.container = document.querySelector('#root');
 
  }
 
  createTemplate(arr) {
    const markup = arr.reduce((acc, item) => acc + card(item), '');
-
+   
    this.refs.container.insertAdjacentHTML('afterbegin', markup);
  }
 }

84bb4a0242aa95863611fbd4c1cd893da62d3b36
Andrey Kornyushko <kap86@ukr.net>
2018-10-24 13:50:51 +0300

    adds slider

--- project/src/js/controller.js
+++ project/src/js/controller.js
@@ -20,6 +20,50 @@ export default class Controller {
       'click',
       this.onClickShowMore.bind(this),
     );
+
+    this._view.refs.container.addEventListener(
+      'click',
+      this.onClickShowSlider.bind(this),
+    );
+
+    // this.slide();
+
+    this._view.refs.prew.addEventListener('click', this.onclickPrew.bind(this));
+
+    this._view.refs.next.addEventListener('click', this.onclickNext.bind(this));
+  }
+
+  slide() {
+    const slides = document.querySelectorAll('.slider__list-item');
+    // for (let i = 0; i < this._view.refs.slides.length; i++) {
+    //   this._view.refs.slides[i].style.zIndex =
+    //     this._view.refs.slides.length - i;
+    // }
+    for (let i = 0; i < slides.length; i++) {
+      slides[i].style.zIndex = slides.length - i;
+    }
+  }
+
+  onclickNext(evt) {
+    this.slide();
+    console.log('next');
+    const activeEl = document.querySelector('.active');
+    if (activeEl.nextElementSibling) {
+      activeEl.style.left = '-100%';
+      activeEl.classList.remove('active');
+      activeEl.nextElementSibling.classList.add('active');
+    }
+  }
+
+  onclickPrew(evt) {
+    this.slide();
+    console.log('prew');
+    const activeEl = document.querySelector('.active');
+    if (activeEl.previousElementSibling) {
+      activeEl.previousElementSibling.style.left = '0%';
+      activeEl.classList.remove('active');
+      activeEl.previousElementSibling.classList.add('active');
+    }
   }
 
   onClickSearch(evt) {
@@ -49,5 +93,16 @@ export default class Controller {
       this._view.createTemplate(data);
       this._view.refs.form.reset();
     });
+  }
+
+  onClickShowSlider(event) {
+    event.preventDefault();
+    console.log('event', event);
+    const target = event.target;
+    const nodeName = target.nodeName;
+    if (nodeName !== 'IMG') return;
+    this._view.refs.page.classList.add('show-slider');
+    console.log('slider open');
+    this._view.createSliderTemplate(this._model._items);
   }
 }
--- project/src/js/view.js
+++ project/src/js/view.js
@@ -1,9 +1,11 @@
 import card from '../templates/cards.hbs';
+import mcard from '../templates/mcard.hbs';
 
 export default class View {
   constructor() {
     this.refs = {};
 
+    this.refs.page = document.querySelector('.page');
     this.refs.selectedBtn = document.querySelector('.selected-btn');
     this.refs.form = document.querySelector('.js-form');
     this.refs.input = document.querySelector('input[name=query]');
@@ -12,11 +14,22 @@ export default class View {
     this.refs.list = document.querySelector('.list__img-card');
     this.refs.images = document.querySelectorAll('.list__img-card');
     this.refs.container = document.querySelector('#root');
+    this.refs.sliderContainer = document.querySelector('#slider');
+    this.refs.slider = document.querySelector('.slider');
+    this.refs.prew = document.querySelector('.prew');
+    this.refs.next = document.querySelector('.next');
+    this.refs.slides = document.querySelectorAll('.slider__list-item');
   }
 
   createTemplate(arr) {
     const markup = arr.reduce((acc, item) => acc + card(item), '');
 
     this.refs.container.insertAdjacentHTML('beforeend', markup);
+  }
+
+  createSliderTemplate(arr) {
+    const markup = arr.reduce((acc, item) => acc + mcard(item), '');
+
+    this.refs.sliderContainer.insertAdjacentHTML('beforeend', markup);
   }
 }
